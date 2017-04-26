e1059fc2945a65e286f94edb38d070d4c6f09832
jhennies <julianhennies@hotmail.de>
2017-04-26 14:38:49 +0200

    exp170425 splA z1

---
+++ exp_170425_all_samples_lifted_more_trees/run_mc_splA_z1.py
@@ -0,0 +1,39 @@
+
+import os
+
+import sys
+sys.path.append(
+    '/export/home/jhennies/src/nature_methods_multicut_pipeline_devel/nature_methods_multicut_pipeline/software/')
+
+from multicut_src import ExperimentSettings
+
+from pipeline import run_lifted_mc
+
+# TODO Change here
+from init_exp_splA_z1 import meta_folder, experiment_folder
+rf_cache_folder = os.path.join(meta_folder, 'rf_cache')
+
+
+if __name__ == '__main__':
+
+    # TODO Change here
+    from init_exp_splA_z1 import test_name, train_name
+
+    ExperimentSettings().rf_cache_folder = rf_cache_folder
+    ExperimentSettings().anisotropy_factor = 10.
+    ExperimentSettings().use_2d = False
+    ExperimentSettings().n_threads = 30
+    ExperimentSettings().n_trees = 500
+    ExperimentSettings().solver = 'multicut_fusionmoves'
+    ExperimentSettings().verbose = True
+    ExperimentSettings().weighting_scheme = 'z'
+    ExperimentSettings().lifted_neighborhood = 3
+
+    # TODO Change here for changing half
+    run_lifted_mc(
+        meta_folder,
+        train_name,
+        test_name,
+        experiment_folder + 'result.h5',
+        'z/1/data'
+    )
