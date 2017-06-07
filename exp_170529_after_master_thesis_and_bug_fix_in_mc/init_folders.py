896f637a5c0fec3c07b67069d38e7cf028c40307
jhennies <julianhennies@hotmail.de>
2017-06-07 10:42:18 +0200

    aborted pipeline

---
+++ exp_170529_after_master_thesis_and_bug_fix_in_mc/init_folders.py
@@ -0,0 +1,11 @@
+
+import os
+
+# TODO Change here when switching server
+# slimchicken: .../localdata02/...
+# mipserver01: .../localdata01/...
+source_folder = '/mnt/localdata02/jhennies/neuraldata/cremi_2016/170321_resolve_false_merges/'
+
+project_folder = '/mnt/localdata01/jhennies/neuraldata/results/multicut_workflow_2/170529_after_master_thesis_and_bug_fix_in_mc/'
+
+train_folder = os.path.join(project_folder, 'train_sets_cache/')
---
+++ exp_170529_after_master_thesis_and_bug_fix_in_mc/run_mc_splB_z0.py
@@ -0,0 +1,43 @@
+
+import os
+
+import sys
+sys.path.append(
+    '/export/home/jhennies/src/nature_methods_multicut_pipeline_devel/nature_methods_multicut_pipeline/software/')
+
+from multicut_src import ExperimentSettings
+
+from pipeline import run_lifted_mc
+
+# TODO Change here
+from init_exp_splB_z0 import meta_folder, experiment_folder
+rf_cache_folder = os.path.join(meta_folder, 'rf_cache')
+
+# TODO Change here when switching half
+result_key = 'z/0/data'
+
+
+if __name__ == '__main__':
+
+    # TODO Change here
+    from init_exp_splB_z0 import test_name, train_names, train_folder
+
+    ExperimentSettings().rf_cache_folder = rf_cache_folder
+    ExperimentSettings().anisotropy_factor = 10.
+    ExperimentSettings().use_2d = False
+    ExperimentSettings().n_threads = 30
+    ExperimentSettings().n_trees = 500
+    ExperimentSettings().solver = 'multicut_fusionmoves'
+    ExperimentSettings().verbose = True
+    ExperimentSettings().weighting_scheme = 'z'
+    ExperimentSettings().lifted_neighborhood = 3
+
+    run_lifted_mc(
+        meta_folder,
+        train_folder,
+        train_names,
+        test_name,
+        experiment_folder + 'result.h5',
+        result_key,
+        pre_save_path=experiment_folder + 'pre_result.h5'
+    )
